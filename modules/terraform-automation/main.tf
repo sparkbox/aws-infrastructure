e05bb934c07527eecc669350eb09f4f3c9903589
Ryan Cromwell <ryan@heysparkbox.com>
2020-08-04 00:06:46 -0400

    feat (Terraform): Permissions to manage users and organizational access

--- modules/terraform-automation/main.tf
+++ modules/terraform-automation/main.tf
@@ -6,3 +6,32 @@ resource "aws_iam_access_key" "access_key" {
   user    = aws_iam_user.user.name
   pgp_key = var.pgp_key
 }
+
+resource "aws_iam_user_policy" "terraform-user-iam-access-policy" {
+  user = aws_iam_user.user.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "iam:*User",
+        "iam:*Group",
+        "iam:GetLoginProfile",
+        "iam:ListAttachedGroupPolicies",
+        "iam:ListAccessKeys",
+        "iam:ListAttachedGroupPolicies",
+        "organizations:ListRoots",
+        "organizations:ListAccounts",
+        "organizations:DescribeOrganization",
+        "organizations:ListOrganizationalUnitsForParent",
+        "organizations:ListAWSServiceAccessForOrganization"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
